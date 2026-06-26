a3d093ca57f904f7b7d331ccce099f6bff2a6dff
Sasha_ML <insomniacml91@gmail.com>
2026-06-27 01:47:39 +0700

    feat: initial ML pipeline for churn predictions

---
+++ train.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report
+
+
+df=pd.read_csv('churns.csv')
+
+df = pd.get_dummies(df, columns=['Contract', 'InternetService'], drop_first=True)
+
+binary_yes_no_cols = [
+    'gender', 'Partner', 'Dependents', 'PhoneService', 'PaperlessBilling',
+    'MultipleLines', 'OnlineSecurity', 'OnlineBackup', 'DeviceProtection', 
+    'TechSupport', 'StreamingTV', 'StreamingMovies'
+]
+
+df[binary_yes_no_cols] = df[binary_yes_no_cols].replace({
+    'Male': 1, 'Female': 0,
+    'Yes': 1, 'No': 0, 'No internet service': 0, 'No phone service': 0
+}).astype('int')
+df['Churn'] = df['Churn'].replace({'Yes': 1, 'No': 0}).astype('int')
+df = pd.get_dummies(df, columns=['PaymentMethod'], drop_first=True)
+
+df = df.drop(columns=['TotalCharges', 'customerID'])
+
+features = df.drop(columns=['Churn']).columns.tolist()
+
+X = df[features]
+y = df['Churn']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = RandomForestClassifier(
+    random_state=42, 
+    class_weight='balanced', 
+    max_depth=5, 
+    n_estimators=100 
+)
+
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+
+print(f'Точность модель: {accuracy*100:.1f}%')
+print(classification_report(y_test, y_pred))
